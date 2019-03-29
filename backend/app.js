55d2e87b03c4285025336ef89cf1288d91be0c06
LIM KWANG KYU <kk.lim@gsshop.com>
2019-03-29 15:18:41 +0900

    swagger

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,41 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 
 const winston = require('./config/winston');
 const indexRouter = require('./routes/index');
 
 
+// Swagger definition
+// You can set every attribute except paths and swagger
+// https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
+const swaggerDefinition = {
+  info: { // API informations (required)
+    title: 'Auth Service', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'Auth API' // Description (optional)
+  },
+  host: 'localhost:3000', // Host (optional)
+  basePath: '/' // Base path (optional)
+};
+
+// Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  apis: ['./routes/index.js', './users/index.js', './roles/index.js']
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
